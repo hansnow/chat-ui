e68ba54ab8c232a922f1549735e3011b1764b9fb
hansnow <hansnow2012@gmail.com>
2023-03-06 11:32:23 +0800

    feat: don't rely on ip address

--- services/index.ts
+++ services/index.ts
@@ -1,7 +1,9 @@
 export const usernameName = "auth-name";
 export const authTokenName = "auth-token";
 
-const apiServer = "http://43.156.82.111:8081";
+// const apiServer = "http://43.156.82.111:8081";
+// 不依赖具体的服务器
+const apiServer = "";
 
 export const isTokenValid = async (username: string, password: string) => {
   const resp = await fetch(`${apiServer}/waj`, {
